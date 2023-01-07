a6ffd7e2794387c987016284d46ba29f616d4f73
wellyngton-souza <wellyngtonteixeira12@gmail.com>
2023-01-07 12:03:51 -0300

    added pawn movement

--- components/Tabuleiro/index.js
+++ components/Tabuleiro/index.js
@@ -26,7 +26,9 @@ gerarTabuleiro = () =>{
                     quadrado.style.color = 'black';
                 }
             }
-            quadrado.innerHTML = `i${i}j${j}`;
+
+            //exibe o id (visualmente)
+            //quadrado.innerHTML = `i${i}j${j}`;
 
             //gerarPeao
             if(i == 1){
@@ -38,6 +40,7 @@ gerarTabuleiro = () =>{
             }
             if(i == 6){
                 let peao = document.createElement('img');
+                peao.setAttribute("id", `peaobranco`);
                 peao.setAttribute("class", `peca pecabranca`);
                 peao.setAttribute("src", `./assets/dama.png`);
                 quadrado.appendChild(peao);
@@ -114,19 +117,130 @@ gerarTabuleiro = () =>{
 document.addEventListener('load', gerarTabuleiro());
 
 var tabuleiroDom = document.querySelector("#tabuleiro");
-var peao = document.querySelector("#peao");
-peao.style.backgroundColor = "blue";
 
-function escolherPeca(e) {
+const apagapeca = (pecadeletada) =>{
+    if(document.getElementById('pecaselecionada').innerHTML == '1'){
+        let posivelposicao = pecadeletada.id.split("");
+        let posivelposicaoy = posivelposicao[3];
+        let posivelposicaox = posivelposicao[1];
+        posivelposicao = parseInt(posivelposicaox);
+        posivelposicao+= -1;
+
+        posivelposicao = "i"+posivelposicao+"j"+posivelposicaoy;
+        let pecaremovida = document.getElementById(posivelposicao);
+        console.log("peça removida: " + posivelposicao);
+        pecaremovida.removeChild(pecaremovida.firstChild);
+        //document.getElementById(posivelposicao).removeChild(document.get);
+    }
+    if(document.getElementById('pecaselecionada').innerHTML == '2'){
+        let posivelposicao = pecadeletada.id.split("");
+        let posivelposicaoy = posivelposicao[3];
+        let posivelposicaox = posivelposicao[1];
+        posivelposicao = parseInt(posivelposicaox);
+        posivelposicao++;
+
+        posivelposicao = "i"+posivelposicao+"j"+posivelposicaoy;
+        let pecaremovida = document.getElementById(posivelposicao);
+        console.log("peça removida: " + posivelposicao);
+        pecaremovida.removeChild(pecaremovida.firstChild);
+        //document.getElementById(posivelposicao).removeChild(document.get);
+    }
+}
+
+const voltarcor = (quadrado) =>{
+    let posit = quadrado.id;
+    let posivelposicaoS = posit.split("");
+    let posivelposicaoSX = posivelposicaoS[3];
+    let posivelposicaoSY = posivelposicaoS[1];
+
+    if((parseInt(posivelposicaoSX) + parseInt(posivelposicaoSY)) % 2){
+        quadrado.style.backgroundColor = "black";
+    } else{
+        quadrado.style.backgroundColor = "white";
+    }
+    apagapeca(quadrado);
+}
+
+const escolherPeca = (e) => {
     if(e.target != tabuleiroDom)
     {
-        if (e.target.style.backgroundColor == 'black')
+        if(e.target.id == "peao")
         {
-            e.target.style.backgroundColor = 'white';
-            e.target.style.color = 'black';
-        } else {
-            e.target.style.backgroundColor = 'black';
-            e.target.style.color = 'white';
+            let posivelposicao = e.target.parentNode.id.split("");
+            let posivelposicaoy = posivelposicao[3];
+            let posivelposicaox = posivelposicao[1];
+            posivelposicao = parseInt(posivelposicaox);
+            posivelposicao ++;
+
+            if((posivelposicao + parseInt(posivelposicaoy)) % 2){
+                var arroz = "black";
+            } else{
+                var arroz = "white";
+            }
+
+            console.log("cor armazenada: " + arroz);
+
+            posivelposicao = "i"+posivelposicao+"j"+posivelposicaoy;
+
+            let laranja = document.getElementById('posicao');
+            laranja.innerHTML = posivelposicao;
+            document.getElementById('pecaselecionada').innerHTML = "1";
+
+            if(document.getElementById(posivelposicao).style.backgroundColor == "blue"){
+                document.getElementById(posivelposicao).style.backgroundColor = arroz;
+            } else{
+                document.getElementById(posivelposicao).style.backgroundColor = "blue";
+            }
+        }
+        if(e.target.id == "peaobranco")
+        {
+            let posivelposicao = e.target.parentNode.id.split("");
+            let posivelposicaoy = posivelposicao[3];
+            let posivelposicaox = posivelposicao[1];
+            posivelposicao = parseInt(posivelposicaox);
+            posivelposicao += -1;
+
+            if((posivelposicao + parseInt(posivelposicaoy)) % 2){
+                var arroz = "black";
+            } else{
+                var arroz = "white";
+            }
+
+            console.log("cor armazenada: " + arroz);
+
+            posivelposicao = "i"+posivelposicao+"j"+posivelposicaoy;
+
+            let laranja = document.getElementById('posicao');
+            laranja.innerHTML = posivelposicao;
+            document.getElementById('pecaselecionada').innerHTML = "2";
+
+            if(document.getElementById(posivelposicao).style.backgroundColor == "green"){
+                document.getElementById(posivelposicao).style.backgroundColor = arroz;
+            } else{
+                document.getElementById(posivelposicao).style.backgroundColor = "green";
+            }
+        }
+        if(e.target.style.backgroundColor == "blue")
+        {
+            let posivelposicao = document.querySelector('span').innerHTML;
+            console.log(posivelposicao);
+            let peao = document.createElement('img');
+            peao.setAttribute("id", `peao`);
+            peao.setAttribute("class", `peca`);
+            peao.setAttribute("src", `./assets/dama.png`);
+            document.getElementById(posivelposicao).appendChild(peao);
+            voltarcor(e.target);
+        }
+        if(e.target.style.backgroundColor == "green")
+        {
+            let posivelposicao = document.querySelector('span').innerHTML;
+            console.log("nova posição: " + posivelposicao);
+            let peao = document.createElement('img');
+            peao.setAttribute("id", `peaobranco`);
+            peao.setAttribute("class", `peca pecabranca`);
+            peao.setAttribute("src", `./assets/dama.png`);
+            document.getElementById(posivelposicao).appendChild(peao);
+            voltarcor(e.target);
         }
     }
 }
